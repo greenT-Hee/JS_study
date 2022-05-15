b501b68063609b2a67e49e94627d495633427af9
greenT-Hee <heehee970322@gmail.com>
2022-05-15 17:16:36 +0900

    풀이 업로드

---
+++ 100제.js
@@ -0,0 +1,28 @@
+//11번
+// const level = 5;
+
+// for(let i =1; i <= level; i++){
+//     let tree= '';
+//     //공백증가 for문
+//     for(let k=1; k <= level-i; k++){
+//         tree = tree + ' ';
+//     }
+//     //별 증가 for문
+//     for(let j =1; j <= i*2-1; j++){
+//         tree = tree + '*';
+//     }
+//     console.log(tree);
+// }
+
+//14번 
+// let a = prompt('원하는 숫자를 말해봐')
+// if(a % 3 == 0){
+//     document.write('짝')
+// }else{
+//     document.write(a);
+// }
+
+//15번
+let 이름 = prompt('네 이름이 뭐니??');
+
+document.write('안녕하세요! 저는 ',이름,'입니다.');
