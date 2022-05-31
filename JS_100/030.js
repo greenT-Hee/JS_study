0941b915b6009a1b5acdb56e57502cd297fba51e
greenT-Hee <heehee970322@gmail.com>
2022-05-31 16:28:10 +0900

    0601_resolve

---
+++ JS_100/030.js
@@ -0,0 +1,28 @@
+// 문자 pineapple에는 apple이라는 문자가 숨어 있습니다. 원범이는 이렇듯 문자열 속에 숨어있는 문자를 찾아보려고 합니다.
+// 첫번째 입력에서는 문자열이 입력되고, 두번째에는 찾을 문자가 입력되어야 합니다.
+// 그 문자가 시작하는 index를 반환하는 프로그램을 만들어 주세요
+
+// 입력
+// pineapple is yummy
+// apple
+// 출력
+// 4
+
+
+
+// 내 답안 
+const sentence = prompt('파인앤플은 맛있어를 영어로 입력하시오');
+const word = prompt('사과를 영어로 입력하시오');
+console.log(sentence.indexOf(word));
+
+
+
+
+// 본답안
+const data = prompt('문자열을 입력하세요');
+const word = prompt('찾을 단어를 입력하세요');
+
+console.log(data.indexOf(word)); 
+/* indexOf() 메서드는 호출한 스트링 객체나 배열에서 
+ * 주어진 값과 일치하는 값 혹은 요소의 첫 번째 인덱스를 반환하고 존재하지 않으면 -1을 반환합니다.
+*/
