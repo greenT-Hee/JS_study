c58a9b165e6034b798c84e86aefc88d07f83d223
greenT-Hee <heehee970322@gmail.com>
2023-02-15 18:28:23 +0900

    :bug: 35번 푸는 중

--- 2023-reStudy/1번~50번/31번~40번.js
+++ 2023-reStudy/1번~50번/31번~40번.js
@@ -11,7 +11,57 @@
 
 // 33번 
 // 한 줄에 여러개의 숫자가 입력되면, 역순으로 그 숫자들을 하나씩 출력하는 프로그램을 작성하시오. 
+// 12345
+// 54321
 
-const reverse = () => {
-  
+function ToObj (num) {
+  this.num = num;
 }
+const obj = new ToObj(54321);
+
+ToObj.prototype.toString = function toString () {
+  return `${this.num}`
+}
+
+console.log(obj.toString().split('').reverse().join(',').replaceAll(/,/ig,''));
+
+
+// 34번 
+// 키가 주어지면 순서대로 제대로 섰는지 확인하는 프로그램
+// 입력 : 176 156 155 165 166 169
+// 출력 : NO
+
+// 입력 : 155 156 165 166 169 176
+// 출력 : YES
+
+const heights = (num) => {
+  const sorted = num.split(' ').sort().join(',').replaceAll(/,/ig,' ');
+
+  if(num === sorted) {
+    console.log('YES');
+  } else {
+    console.log('NO')
+  }
+}
+
+heights("155 156 165 166 169 176");
+
+
+// 35번 
+// 2제곱, 3제곱, 4제곱을 할 수 있는 Factory 함수를 만들려고 합니다. 
+// <pass>에 코드를 작성하여 two함수를 완성하세요.
+
+function one(n){
+  console.log(n)
+  function two(){
+  }
+  return two;
+}
+
+const a = one(2);
+const b = one(3);
+const c = one(4);
+
+console.log(a(10));
+console.log(b(10));
+console.log(c(10));
