51dd7616b8b94e4dfaea8d79f829aa751ba087d2
greenT-Hee <heehee970322@gmail.com>
2023-02-19 23:27:22 +0900

    :green_heart: 42번 풀이

--- 2023-reStudy/1번~50번/41번~50번.js
+++ 2023-reStudy/1번~50번/41번~50번.js
@@ -21,4 +21,24 @@ decimal(7);
 
 
 // 42번 
-// 2020년 1월 1일 수요일 
+// 2020년 1월 1일 수요일
+// 2020년 2월은 윤년 (2월 29일)
+function solution (a, b) {
+  const day = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
+  const x = new Date(2020 + "-" + a + "-" + b);
+  return day[x.getDay()]
+}
+
+console.log(solution(3, 22));
+
+// 43번 
+// 숫자를 입력받고 이를 2진수를 바꾸고 그 값을 출력
+function binaryNumber(number) {
+  console.log("10진수", number);
+  let 이진수 = 0;
+  arr.forEach(item => {
+    
+  });
+}
+
+binaryNumber(7536);
